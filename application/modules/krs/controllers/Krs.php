406eea7034e736d5daa398b5956bc30743d7ae2b
fahrijal <61215442+fahrijal-alkuja@users.noreply.github.com>
2022-07-07 17:42:04 +0800

    Update Krs.php

--- application/modules/krs/controllers/Krs.php
+++ application/modules/krs/controllers/Krs.php
@@ -29,7 +29,7 @@ class Krs extends CI_Controller
 		$tahun = $this->input->post('tahun');
 		$periode = $this->input->post('semester');
 		$prodi = $this->input->post('prodi');
-		$this->API = "http://api.unikarta.ac.id/api/krs?API-KEY=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImFsa3VqYSIsImlhdCI6MTU0MTk5MzkzNiwiZXhwIjoxNTQxOTk3NTM2fQ.eeVoobmrGio3Eu3vksKVgp_KGaHbVW2S7zhMsHeagVw&tahun=$tahun&periode=$periode&kode_program_studi=$prodi";
+		$this->API = "http://api.unikarta.ac.id/api/krs?API-KEY=..keydisini&tahun=$tahun&periode=$periode&kode_program_studi=$prodi";
 		$data['krs'] = json_decode($this->curl->simple_get($this->API));
 		$this->load->view('page_krs', $data);
 	}
@@ -40,7 +40,7 @@ class Krs extends CI_Controller
 		$tahun = $this->input->post('tahun');
 		$periode = $this->input->post('semester');
 		$prodi = $this->input->post('prodi');
-		$this->API = "http://api.unikarta.ac.id/api/krs?API-KEY=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImFsa3VqYSIsImlhdCI6MTU0MTk5MzkzNiwiZXhwIjoxNTQxOTk3NTM2fQ.eeVoobmrGio3Eu3vksKVgp_KGaHbVW2S7zhMsHeagVw&tahun=$tahun&periode=$periode&kode_program_studi=$prodi";
+		$this->API = "http://api.unikarta.ac.id/api/krs?API-KEY=..keydisini&tahun=$tahun&periode=$periode&kode_program_studi=$prodi";
 		$hsl = json_decode($this->curl->simple_get($this->API));
 		$no_ins = $no_upd = 0;
 		foreach ($hsl as $data) {
